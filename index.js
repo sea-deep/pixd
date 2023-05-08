5275f69f1818d75787187c6d70a1ba1680dde911
Sea-Deep <122673793+susudeepa@users.noreply.github.com>
2023-05-08 17:40:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -150,6 +150,9 @@ client.on(Events.MessageCreate, async (message) => {
       case 'lapata':
         await lapata(message);
         break;
+      case 'testWel'
+        await sendPajeet(message);
+        break;
       default:
         break;
     }
@@ -209,7 +212,21 @@ client.on(Events.ShardError, (error) => {
 
 client.login(process.env.BOT_TOKEN);
 
-
+async function sendPajeet (message) {
+ let avatar = Jimp.read(message.member.user.displayAvatarURL({
+   extension: 'png',
+   forceStatic: true
+ }));
+ avatar.resize(275, 275).circle();
+ let banner = Jimp.read('https://media.discordapp.net/attachments/1063000940279509022/1105098775652995124/PicsArt_05-08-05.14.41.png');
+ banner.composite(avatar, 50, 80);
+ let buffer = banner.getBufferAsync(Jimp.MIME_PNG);
+ let file = new AttachmentBuilder(buffer, {name: "aagaya_muh_uthake.png"});
+ return message.channel.send({
+   content: `Namaste sirs <@${message.member.user.id}> did poo in the loo`,
+   files: [ file ]
+ });
+}
 async function sendokbb(member) {
   let avatarURL = member.user.displayAvatarURL({
     extension: 'png',
