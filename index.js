e24b8883e24e311eb372bf84b76b6ce1b6a185cc
Sea-Deep <122673793+susudeepa@users.noreply.github.com>
2023-05-30 22:15:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -130,6 +130,9 @@ client.on(Events.MessageCreate, async (message) => {
       case 'seek':
         await seek(message, serverQueue);
         break;
+      case 'animan':
+        await sendAniman(message);
+        break;
       case 'genetics':
         await genetics(message);
         break;
@@ -234,6 +237,73 @@ client.on(Events.ShardError, (error) => {
 
 client.login(process.env.BOT_TOKEN);
 
+
+async function sendAniman(message) {
+  await message.channel.sendTyping();
+  const idArray = message.content.match(/<@(\d+)>/g);
+  let mentions = Array.from(message.mentions.users);
+
+  let sortedMentions = [];
+  for (let i = 0; i < idArray.length; i++) {
+    for (let j = 0; j < mentions.length; j++) {
+      if (idArray[i].includes(mentions[j].id)) {
+        sortedMentions.push(mentions[j]);
+        break;
+      }
+    }
+  }
+
+  mentions = sortedMentions;
+  mentions.sort((a, b) => idArray.indexOf(a.id) - idArray.indexOf(b.id));
+  if (mentions.length !== 4) {
+    return message.reply(`Mention 4 peoples 🤓`);
+  }
+  let bg = await Jimp.read(
+    'https://cdn.discordapp.com/attachments/916697198761234492/1098731085187252344/PicsArt_04-21-03.34.30.png'
+  );
+  bg.resize(720, 762);
+  let avatar1 = await Jimp.read(mentions[0].displayAvatarURL());
+  let avatar2 = await Jimp.read(mentions[1].displayAvatarURL());
+  let avatar3 = await Jimp.read(mentions[2].displayAvatarURL());
+  let avatar4 = await Jimp.read(mentions[3].displayAvatarURL());
+  avatar1.resize(80, 80);
+  avatar2.resize(148, 144);
+  avatar3.resize(123, 112);
+  avatar4.resize(100, 110);
+  const animan = new jimp(720, 762, 00000000)
+    .composite(avatar2, 156, 527)
+    .composite(avatar3, 363, 581)
+    .composite(avatar4, 555, 527)
+    .composite(anim, 0, 0)
+    .composite(avatar1, 291, 38);
+  let buffer = await animan.getBufferAsync(jimp.MIME_PNG);
+
+  const b = [
+    'I put the new Forgis on the Jeep',
+    'I trap until the bloody bottoms is underneath',
+    "'Cause all my niggas got it out the streets",
+    'I keep a hundred racks, inside my jeans',
+    "I remember hittin' the mall with the whole team",
+    "Now a nigga can't answer calls 'cause I'm ballin'",
+    "I was wakin' up gettin' racks in the morning",
+    "I was broke, now I'm rich, these niggs salty",
+    'All this designer on my body got me drip, drip, ayy',
+    "Straight up out the Yaadas, I'm a big Crip",
+    "If I got a pint of lean, I'ma sip, sip",
+    'I run the racks up with my queen like London and Nip',
+    "But I got rich on all these niggas I didn't forget back",
+    "I had to go through the struggle, I didn't forget that",
+    'I hopped inside of the Maybach and now I can sit back',
+    'These Chanel bags is a bad habit, I-I do not know how to act',
+  ];
+
+  const line = b[Math.floor(Math.random() * b.length)];
+  let file = new AttachmentBuilder(buffer, {name: 'animanu'});
+  return message.reply({
+    content: line,
+    files: [file],
+  });
+}
 async function sendokbbl(member) {
 let channel = client.channels.cache.get("804902112700923957");
 channel.send(`ayyo saar **${member.user.tag}** gayaa`);
