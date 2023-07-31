3085e1a99121d623d6de17d1a6290090946f938c
Sea-Deep <122673793+susudeepa@users.noreply.github.com>
2023-07-31 20:42:08 +0530

    Create imgInput.js

---
+++ Interactions/Buttons/Image/imgInput.js
@@ -0,0 +1,33 @@
+export default {
+  name: 'img_input',
+  execute: async (interaction) => {
+      if (interaction.message.mentions.users.first().id === interaction.user.id) { 
+     await interaction.showModal({ 
+       custom_id: `imgInputForm`,
+       title: `Jump to a page.`, 
+       components: [ 
+         { 
+           type: 1,
+           components: [ 
+             { 
+               type: 4, 
+               custom_id: 'input', 
+               label: `What page would you like to go?`, 
+               style: 1,
+               min_length: 1, 
+               max_length: 3, 
+               placeholder: '84', 
+               required: true, 
+             }, 
+           ], 
+         }, 
+       ], 
+     }); 
+   } else { 
+     await interaction.reply({ 
+       content: '❌ *This is not your message.*', 
+       ephemeral: true, 
+     }); 
+   }
+  }
+}
