4ff3b30a8572225bc06db61d99735429ba12e7ae
Sea-Deep <122673793+susudeepa@users.noreply.github.com>
2023-08-04 18:50:45 +0530

    Update imgForm.js

--- Interactions/Modals/imgForm.js
+++ Interactions/Modals/imgForm.js
@@ -1,52 +1,53 @@
-import { Client } from 'discord.js';
+import { Client } from "discord.js";
 
 export default {
   name: 'imgInputForm',
-
   /**
     * @param {Client} client
     */
-  async execute(interaction, client) {
-   const regex = /`([^`]+)`/;
-const matches = interaction.message.content.match(regex);
-  const total = parseInt(matches[1].split('/')[1]) - 1;
-  const images = client.keyv.get(interaction.message.id);
- //console.log(images)
-  let val = interaction.fields.getTextInputValue('input');
-   let valueee = Number(val)
- //   console.log(val, valueee)
-   if (isNaN(valueee) || valueee >= total) {
-     return interaction.reply({
-       content: "Not a valid number!",
-       ephemeral: true
-     })
-   }
-    const current = parseInt(matches[1].split('/')[0]) - 1;
-    let next = valueee;
+  execute: async (interaction, client) => {   
+    const regex = /`([^`]+)`/; 
+   const matches = interaction.message.content.match(regex); 
+   const total = parseInt(matches[1].split('/')[1]) - 1; 
+   const images = client.keyv.get(interaction.message.id); 
+   let val = interaction.fields.getTextInputValue('input'); 
+    let valueee = Number(val);
+    if (isNaN(valueee) || valueee >= total) { 
+      return interaction.reply({ 
+        content: "Not a valid number!", 
+        ephemeral: true 
+      });
+    } 
+  let current = parseInt(matches[1].split('/')[0]) - 1;
+  let next = valueee;
   let image= images[next];
   let msg = interaction.message;
-  let content = msg.content.split('\n');
-    content[1] = content[1].replace('`'+(current+1), '`'+(next+1));
-   
- const embed = {
-     title: image.origin.title,
-     description: `via **[${image.origin.website.name}](https://${image.origin.website.domain})**`,
+    const embed = {
+     title: message.embeds[0].title,
+     description: `**[${image.title}](${image.originalUrl})**`,
      image: {
           url: image.url,
           height: image.height,
           width: image.width
         },
-             color: 0x666,
-     url: image.origin.website.url
+      color: getColor(image),
+      footer: {
+        text: msg.footer.replace('`'+(current+1), '`'+(next+1))
+      }
      };
-        await interaction.deferUpdate();
+    await interaction.deferUpdate();
       await interaction.message.edit(
       {
-          
-          content: content.join('\n'),
+          content: '',
           embeds: [embed],
           components: msg.components 
           }); 
-    
   }
+};
+function getColor(image) {
+    const r = image.averageColorObject.r;
+    const g = image.averageColorObject.g;
+    const b = image.averageColorObject.b;
+    const color = (r << 16) | (g << 8) | b;  
+    return color;  
 }
