525d0cb9001b1d8479a1fedb40ea7084bfb29e83
Sea-Deep <122673793+susudeepa@users.noreply.github.com>
2023-08-04 19:26:25 +0530

    Update imgRandom.js

--- Interactions/Buttons/Image/imgRandom.js
+++ Interactions/Buttons/Image/imgRandom.js
@@ -10,7 +10,7 @@ export default {
     const images = client.keyv.get(interaction.message.id);
     
    const regex = /`([^`]+)`/;
-const matches = interaction.message.content.match(regex);
+const matches = interaction.message.embeds[0].footer.text.match(regex);
   const current = parseInt(matches[1].split('/')[0]) - 1;
   let next = Math.floor(Math.random() * images.length);
   let image= images[next];
