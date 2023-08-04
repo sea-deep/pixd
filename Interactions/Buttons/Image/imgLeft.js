e1d34190ef8f8c5b863ef442b67e737c99a26997
Sea-Deep <122673793+susudeepa@users.noreply.github.com>
2023-08-04 19:26:46 +0530

    Update imgLeft.js

--- Interactions/Buttons/Image/imgLeft.js
+++ Interactions/Buttons/Image/imgLeft.js
@@ -10,7 +10,7 @@ export default {
     const images = client.keyv.get(interaction.message.id);
     
    const regex = /`([^`]+)`/;
-const matches = interaction.message.content.match(regex);
+const matches = interaction.message.embeds[0].footer.text.match(regex);
   const current = parseInt(matches[1].split('/')[0]) - 1;
   let next = current==0 ? images.length - 1 : current - 1;
   let image= images[next];
