630129b829ba5d79b049afb0f2495da3e7762e31
Sea-Deep <122673793+susudeepa@users.noreply.github.com>
2023-08-06 02:27:10 +0530

    Update imgForm.js

--- Interactions/Modals/imgForm.js
+++ Interactions/Modals/imgForm.js
@@ -1,53 +1,61 @@
-import { Client } from "discord.js";
+import { Client } from "discord.js"; 
+  
+export default { 
+  name: 'imgInputForm', 
+  /** 
+    * @param {Client} client 
+    */ 
+  execute: async (interaction, client) => {    
+    try {
+      const regex = /`([^`]+)`/;  
+      const matches = interaction.message.embeds[0].footer.text.match(regex);  
+      const total = parseInt(matches[1].split('/')[1]) - 1;  
+      const images = await client.keyv.get(interaction.message.id);  
+      const val = interaction.fields.getTextInputValue('input');  
+      const valueee = Number(val); 
+      
+      if (isNaN(valueee) || valueee >= total || valueee < 0) {  
+        return interaction.reply({  
+          content: "Not a valid number!",  
+          ephemeral: true  
+        }); 
+      }  
 
-export default {
-  name: 'imgInputForm',
-  /**
-    * @param {Client} client
-    */
-  execute: async (interaction, client) => {   
-    const regex = /`([^`]+)`/; 
-   const matches = interaction.message.embeds[0].footer.text.match(regex); 
-   const total = parseInt(matches[1].split('/')[1]) - 1; 
-   const images = client.keyv.get(interaction.message.id); 
-   let val = interaction.fields.getTextInputValue('input'); 
-    let valueee = Number(val);
-    if (isNaN(valueee) || valueee >= total) { 
-      return interaction.reply({ 
-        content: "Not a valid number!", 
-        ephemeral: true 
+      const current = parseInt(matches[1].split('/')[0]) - 1; 
+      const next = valueee; 
+      const image = images[next]; 
+      const msg = interaction.message; 
+      
+      const embed = { 
+        title: msg.embeds[0].title, 
+        description: `**[${image.title}](${image.originalUrl})**`, 
+        image: { 
+          url: image.url, 
+          height: image.height, 
+          width: image.width 
+        }, 
+        color: getColor(image), 
+        footer: { 
+          text: msg.embeds[0].footer.text.replace('`' + (current + 1), '`' + (next + 1)) 
+        } 
+      }; 
+      
+      await interaction.deferUpdate(); 
+      await interaction.message.edit({ 
+        content: '', 
+        embeds: [embed], 
+        components: msg.components  
       });
-    } 
-  let current = parseInt(matches[1].split('/')[0]) - 1;
-  let next = valueee;
-  let image= images[next];
-  let msg = interaction.message;
-    const embed = {
-     title: interaction.message.embeds[0].title,
-     description: `**[${image.title}](${image.originalUrl})**`,
-     image: {
-          url: image.url,
-          height: image.height,
-          width: image.width
-        },
-      color: getColor(image),
-      footer: {
-          text: msg.embeds[0].footer.text.replace('`' + (current + 1), '`' + (next + 1))
-        }
-     };
-    await interaction.deferUpdate();
-      await interaction.message.edit(
-      {
-          content: '',
-          embeds: [embed],
-          components: msg.components 
-          }); 
-  }
-};
-function getColor(image) {
-    const r = image.averageColorObject.r;
-    const g = image.averageColorObject.g;
-    const b = image.averageColorObject.b;
-    const color = (r << 16) | (g << 8) | b;  
-    return color;  
+    } catch (error) {
+      console.error("An error occurred:", error);     
+    }
+  } 
+}; 
+
+function getColor(image) { 
+  const r = image.averageColorObject.r; 
+  const g = image.averageColorObject.g; 
+  const b = image.averageColorObject.b; 
+  const color = (r << 16) | (g << 8) | b;   
+  return color;   
 }
