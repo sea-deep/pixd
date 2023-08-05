eb5f47e447c07cc0c2d48f9aeef23a9634a03ae7
Sea-Deep <122673793+susudeepa@users.noreply.github.com>
2023-08-06 02:45:44 +0530

    Update ready.js

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -4,9 +4,7 @@ import chalk from 'chalk';
 export default {
   event: "ready",
   once: true,
-  /**
-  * @param {Client} client
-  */
+
   execute(client) {
     console.log(`[${chalk.blue("INFO")}] - Logged in as: ${chalk.greenBright(client.user.tag)}`);
 
