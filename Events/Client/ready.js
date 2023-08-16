6006d6557b3111607178de4e94e97a2c65209eaf
Sea-Deep <122673793+susudeepa@users.noreply.github.com>
2023-08-17 01:41:32 +0530

    Update ready.js

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -1,14 +1,19 @@
-import { Client } from "discord.js";
+import { Client, ActivityType } from "discord.js";
 import chalk from 'chalk';
 
 export default {
   event: "ready",
-  once: false,
+  once: true,
   /**  
   * @param {Client} client     
   */ 
   execute: async (client) => {
     process.stdout.write(`[${chalk.blue("INFO")}] - Logged in as: ${chalk.greenBright(client.user.tag)}\n`);
+      let status = `p!help or /help; 
+      await client.user.setActivity({
+       name: `${status}`,
+       type: ActivityType.Listening 
+     });
   },
 };
 
