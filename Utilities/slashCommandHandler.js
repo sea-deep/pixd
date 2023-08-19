d921c564bb4ddffa109ceeb4176dac7d506a1334
Sea-Deep <122673793+susudeepa@users.noreply.github.com>
2023-08-20 02:09:02 +0530

    Update slashCommandHandler.js

--- Utilities/slashCommandHandler.js
+++ Utilities/slashCommandHandler.js
@@ -3,7 +3,6 @@ import pkg from "glob";
 import { promisify } from "util";
 import { pathToFileURL } from "url";
 import { client } from "../index.js";
-import fetch from "node-fetch";
 
 const { glob } = pkg;
 const proGlob = promisify(glob);
@@ -13,8 +12,6 @@ try {
   client.subCommands.clear();
 
   const Files = await proGlob(`${process.cwd().replace(/\\/g, "/")}/Interactions/SlashCommands/**/*.js`);
-
-  client.interactionsArray = [];
 
   for (let i = 0; i < Files.length; i++) {
     Files[i] = pathToFileURL(Files[i]);
@@ -26,43 +23,9 @@ try {
     }
 
     client.slashCommands.set(interaction.data.name, interaction);
-    client.interactionsArray.push(interaction.data);
   }
 
-  await registerSlashCommands(client.interactionsArray);
 } catch (err) {
   const errorOutput = `[${chalk.red("SlashCommandHandler")}] - ${err}`;
   process.stderr.write(`${errorOutput}\n`);
-}
-
-
-
-// Helper API function 
-async function registerSlashCommands(commandsArray) {
-  const clientId = "1026234292017299586";
-  const url = `https://discord.com/api/v10/applications/${clientId}/commands`;
-  const headers = {
-    "Authorization": `Bot ${process.env.TOKEN}`,
-    "Content-Type": "application/json",
-  };
-
-  try {
-    const response = await fetch(url, {
-      method: "PUT",
-      headers,
-      body: JSON.stringify(commandsArray),
-    });
-
-    if (response.ok) {
-      const infoMessage = `[${chalk.blue("INFO")}] - Slash Command Registered!`;
-      process.stdout.write(`${infoMessage}\n`);
-    } else {
-      const errorMessage = await response.text();
-      const errorOutput = `[${chalk.red("SlashCommandHandler")}] - ${errorMessage}`;
-      process.stderr.write(`${errorOutput}\n`);
-    }
-  } catch (err) {
-    const errorOutput = `[${chalk.red("SlashCommandHandler")}] - ${err}`;
-    process.stderr.write(`${errorOutput}\n`);
-  }
 }
