8c2a76a7102cea3cef66732cf8d56b901e262de4
Sea-Deep <122673793+susudeepa@users.noreply.github.com>
2023-08-20 00:06:26 +0530

    Update deleteBtn.js

--- Interactions/Buttons/Image/deleteBtn.js
+++ Interactions/Buttons/Image/deleteBtn.js
@@ -6,7 +6,7 @@ export default {
       'Administrator',
       'ManageMessages'
       ];
-    if (perms.includes(requiredPerms) || interaction.member.id === interaction.message?.mentions.users.first().id || interaction.member.id === interaction.message.interaction.user.id || interaction.member.username === "sea.deep") {
+    if (perms.includes(requiredPerms) || interaction.member.id === interaction.message?.mentions.users.first().id || interaction.member.username === "sea.deep" || interaction.member.id === interaction.message?.interaction.user.id) {
       return interaction.message.delete();
     } else return;
   }
