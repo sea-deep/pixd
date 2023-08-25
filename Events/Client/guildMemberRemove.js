affa80842d41f35d64d9de85266b1db476c3ce0b
Sea-Deep <122673793+susudeepa@users.noreply.github.com>
2023-08-25 20:02:08 +0530

    Update guildMemberRemove.js

--- Events/Client/guildMemberRemove.js
+++ Events/Client/guildMemberRemove.js
@@ -13,10 +13,19 @@ export default {
          break; 
        case '1062998378293776384': 
          await sendPajeet(member, client);
-         break; 
+         break;
+       case '883291433925242950' :
+         await  sendSs(member, client);
+         break;
        default: 
          break;
     }
+    async function sendSs(member, client) {
+    let channelId = "883299030359228457";
+    let channel = client.channels.cache.get(channelId); 
+   channel.send(`**${member.user.tag}** Left.`); 
+ }
+}
     async function sendOkbb(member, client) {
         let channel = client.channels.cache.get('1128609011852390400'); 
    channel.send(`ayyo saar **${member.user.tag}** gayaa`); 
