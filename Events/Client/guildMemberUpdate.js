46aaffb4c721ef4022fa13717990cc9eadf47a20
Sea-Deep <122673793+susudeepa@users.noreply.github.com>
2023-08-30 10:15:56 +0530

    Create guildMemberUpdate.js

---
+++ Events/Client/guildMemberUpdate.js
@@ -0,0 +1,17 @@
+import { Client } from "discord.js"; 
+ import chalk from 'chalk'; 
+  
+ export default { 
+   event: "guildMemberUpdate",
+   /**   
+   * @param {Client} client      
+   */  
+   execute: async (oldMember, newMember, client) => { 
+    if (oldMember.guild.id !== "804902112700923954") return;     
+    if (oldMember.premiumSinceTimestamp == newMember.premiumSinceTimestamp) return;
+
+   let channelId = "1128597323895808020";
+   let channel = client.channels.cache.get(channelId);
+   return channel.send(`<@${newMember.id}> just boosted the server <:adanisama:1048862984333692938>`)
+   }, 
+ };
