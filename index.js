bed3405d1fda100a95b04df3361bbbdcb63750b3
Sea-Deep <122673793+susudeepa@users.noreply.github.com>
2023-10-09 20:18:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,61 @@
-#!/usr/bin/env node
-
-// Load environment variables from .env file
-import 'dotenv/config';
-
-// Import necessary modules from discord.js and your own utility functions
-import {
-  Client,
-  GatewayIntentBits,
-  Partials,
-  Collection
-} from 'discord.js';
-import { KeyValueStore, sleep } from './Helpers/helperUtil.js';
-
-// Create a Discord client instance
-export const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildVoiceStates,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers,
-  ],
-  partials: [Partials.Channel],
-});
-
-// Setting up global collections for various purposes
-client.prefixCommands = new Collection();
-client.slashCommands = new Collection();
-client.subCommands = new Collection();
-client.buttons = new Collection();
-client.modals = new Collection();
-client.messageSelectMenus = new Collection();
-client.stringSelectMenus = new Collection();
-client.queue = new Collection();
-client.keyv = new KeyValueStore();
-client.sleep = sleep;
-
-// Import various utility modules
-import './Utilities/webpageHandler.js';
-import './Utilities/slashCommandHandler.js';
-import './Utilities/prefixCommandHandler.js';
-import './Utilities/eventHandler.js';
-import './Utilities/buttonHandler.js';
-import './Utilities/modalHandler.js';
-import './Utilities/stringSelectMenuHandler.js';
-import './Utilities/messageSelectMenuHandler.js';
-import './Utilities/registerCommands.js';
-
-// Logging in to the bot with your token
-client.login(process.env.TOKEN);
-
-// Set the current time for uptime tracking
-client.keyv.set('uptime', Date.now());
+#!/usr/bin/env node 
+ import 'dotenv/config'; 
+  
+ import {Client, GatewayIntentBits, Partials, Collection} from 'discord.js'; 
+ import {KeyValueStore, sleep} from './Helpers/helperUtil.js'; 
+  
+ export const client = new Client({ 
+   intents: [ 
+     GatewayIntentBits.Guilds, 
+     GatewayIntentBits.GuildMessages, 
+     GatewayIntentBits.GuildVoiceStates, 
+     GatewayIntentBits.MessageContent, 
+     GatewayIntentBits.GuildMembers, 
+   ], 
+   ws: {properties: {browser: 'Discord iOS'}}, 
+   partials: [Partials.Channel], 
+ }); 
+  
+ // Setting a Global Collection for Commands, Aliases, Buttons & Interactions and more 
+ client.prefixCommands = new Collection(); 
+ client.slashCommands = new Collection(); 
+ client.subCommands = new Collection(); 
+ client.buttons = new Collection(); 
+ client.modals = new Collection(); 
+ client.messageSelectMenus = new Collection(); 
+ client.stringSelectMenus = new Collection(); 
+ client.queue = new Collection(); 
+ client.keyv = new KeyValueStore(); 
+ client.sleep = sleep; 
+  
+ // Website Handler 
+ import('./Utilities/webpageHandler.js'); 
+  
+ // Slash Command Handler 
+ import('./Utilities/slashCommandHandler.js'); 
+  
+ // Prefix Command Handler 
+ import('./Utilities/prefixCommandHandler.js'); 
+  
+ // Event Handler 
+ import('./Utilities/eventHandler.js'); 
+  
+ // Button Handler 
+ import('./Utilities/buttonHandler.js'); 
+  
+ // Modal Handler 
+ import('./Utilities/modalHandler.js'); 
+  
+ // String Select Menu Handler 
+ import('./Utilities/stringSelectMenuHandler.js'); 
+  
+ // Messave Select Menu Handler 
+ import('./Utilities/messageSelectMenuHandler.js'); 
+  
+ // Register application commands 
+ import('./Utilities/registerCommands.js'); 
+  
+ // Logging in to the bot.. 
+ client.login(process.env.TOKEN); 
+ // Setting current time to present the Uptiime 
+ client.keyv.set('uptime', Date.now());
