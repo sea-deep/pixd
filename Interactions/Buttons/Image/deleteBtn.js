36be50e33d359d6fc8002d5630eaf6f0c6ba09f9
Sea-Deep <122673793+susudeepa@users.noreply.github.com>
2023-10-24 21:23:27 +0530

    Update deleteBtn.js

--- Interactions/Buttons/Image/deleteBtn.js
+++ Interactions/Buttons/Image/deleteBtn.js
@@ -1,10 +1,6 @@
-import { Client } from "discord.js";
-
 export default {
   name: 'delete-btn',
-  /**
-  * @param { Client } client
-  */
+
   execute: async (interaction, client) => {
     const perms = interaction.member.permissions.toArray();
     const requiredPerms = [
@@ -12,7 +8,6 @@ export default {
       'ManageMessages'
       ];
     if (perms.includes(requiredPerms) || interaction.member.id === interaction.message.mentions?.users.first().id || interaction.member.user.username === "sea.deep" || interaction.member.id === interaction.message.interaction?.user.id) {
-      await client.keyv.delete(interaction.message.id);
       return interaction.message.delete();
     } else return;
   }
