cb53d3aafa1e74ac651f1e7b58bbafd671fb9f8d
Sea-Deep <122673793+susudeepa@users.noreply.github.com>
2023-12-17 13:19:24 +0530

    Update ud_left.js

--- Interactions/Buttons/UD/ud_left.js
+++ Interactions/Buttons/UD/ud_left.js
@@ -1,11 +1,75 @@
 export default {
   name: 'ud_left',
   execute: (interaction) => {
-    const msg= interaction.message;
+    const msg = interaction.message;
     const btn = msg.components[0].components[1].label;
     const currentPage = parseInt(btn.split("/")[0]) - 1;
     const maxPage = parseInt(btn.split("/")[1]) - 1;
     let goto = currentPage - 1;
     if(currentPage == 0) {goto = maxPage}
+
+    let res = await urban.search(msg.embeds[0].title);
+    let def = res.list[goto];
+    await interaction.reply({
+      content: "",
+      tts: false,
+      components: [
+  {
+    type: 1,
+    components: [
+      {
+        style: 1,
+        custom_id: `ud_left`,
+        disabled: false,
+        emoji: {
+          id: null,
+          name: `◀`,
+        },
+        type: 2,
+      },
+      {
+        style: 2,
+        label: `1/${res.list.length}`,
+        custom_id: `nulll`,
+        disabled: true,
+        type: 2,
+      },
+      {
+        style: 1,
+        custom_id: `ud_right`,
+        disabled: false,
+        emoji: {
+          id: null,
+          name: `▶`,
+        },
+        type: 2,
+      },
+    ],
+  },
+],
+      embeds: [
+        {
+          type: "rich",
+          title: def.word,
+          description: def.definition,
+          color: 0x6969,
+          fields: [
+            {
+              name: "Example:",
+              value: def.example,
+            },
+          ],
+          author: {
+            name: def.author,
+            url: `https://urbandictionary.com/`,
+            icon_url: `https://images.crunchbase.com/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/u8zidc2jnlhz3n1dcbrr`,
+          },
+          footer: {
+            text: `👍:${def.thumbs_up} | 👎:${def.thumbs_down}`,
+          },
+          url: def.permalink,
+        },
+      ],
+    });
    }
 }
