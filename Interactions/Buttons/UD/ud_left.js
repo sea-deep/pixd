4961caab22095ad8fc4ffd08b096d43440328c8b
Sea-Deep <122673793+susudeepa@users.noreply.github.com>
2023-12-17 13:15:03 +0530

    Update ud_left.js

--- Interactions/Buttons/UD/ud_left.js
+++ Interactions/Buttons/UD/ud_left.js
@@ -1,7 +1,11 @@
 export default {
-  name: 'test',
+  name: 'ud_left',
   execute: (interaction) => {
     const msg= interaction.message;
-    const currentPage = 
+    const btn = msg.components[0].components[1].label;
+    const currentPage = parseInt(btn.split("/")[0]) - 1;
+    const maxPage = parseInt(btn.split("/")[1]) - 1;
+    let goto = currentPage - 1;
+    if(currentPage == 0) {goto = maxPage}
    }
 }
