f22e822ea034e5cfb3577aae73c6d1ffe9b68e1a
Sea-Deep <122673793+susudeepa@users.noreply.github.com>
2023-12-17 15:51:25 +0530

    Update ud_right.js

--- Interactions/Buttons/UD/ud_right.js
+++ Interactions/Buttons/UD/ud_right.js
@@ -33,7 +33,7 @@ export default {
               },
               {
                 style: 2,
-                label: `${goto}/${res.list.length}`,
+                label: `${goto+1}/${res.list.length}`,
                 custom_id: `nulll`,
                 disabled: true,
                 type: 2,
