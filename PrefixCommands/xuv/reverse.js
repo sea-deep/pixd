e48fb2ab52a15ebb9cb05ced2afc4d52ed7f4be6
Sea-Deep <122673793+susudeepa@users.noreply.github.com>
2023-12-23 13:48:19 +0530

    Update xuv

---
+++ PrefixCommands/xuv/reverse.js
@@ -0,0 +1,129 @@
+import { Client, Message } from "discord.js";
+import {GOOGLE_IMG_INVERSE_ENGINE_URL} from "google-img-scrap";
+import { getCaptionInput } from "../../Helpers/helpersImage.js";
+export default {
+  name: "reverse",
+  description: "Search image from Google.",
+  aliases: ["rev", "lens"],
+  usage: "",
+  guildOnly: true,
+  args: false,
+  permissions: {
+    bot: [],
+    user: [],
+  },
+  /**
+    * @param {Message} message
+    * @param {Client} client
+    */
+  execute: async (message, args, client) => {
+   const mseg = await message.reply('Searching <a:Searching:1142532717406322809>');
+   const images = await GOOGLE_IMG_INVERSE_ENGINE_URL(
+      getCaptionInput(message),
+      { limit: 5 }
+    );
+    client.keyv.set(mseg.id, images.result, 30);
+   let img = images.result[0]; 
+   const msg = {
+     failIfNotExists: true,
+     content: ``, 
+     tts: false, 
+     components: [ 
+       { 
+         type: 1, 
+         components: [ 
+           { 
+             style: 2, 
+             custom_id: 'img_left', 
+             disabled: false, 
+             emoji: { 
+               id: null, 
+               name: '◀️' 
+             }, 
+             type: 2 
+           }, 
+           { 
+             style: 2, 
+             custom_id: 'img_right', 
+             disabled: false, 
+             emoji: { 
+               id: null, 
+               name: '▶️' 
+             }, 
+             type: 2 
+           }, 
+           { 
+             style: 1, 
+             custom_id: 'img_random', 
+             disabled: false, 
+             emoji: { 
+               id: null, 
+               name: '🔀' 
+             }, 
+             type: 2 
+           }, 
+           { 
+             style: 1, 
+             custom_id: 'img_input', 
+             disabled: false, 
+             emoji: { 
+               id: null, 
+               name: '🔢' 
+             }, 
+             type: 2 
+           }, 
+           { 
+             style: 4, 
+             custom_id: 'delete-btn', 
+             disabled: false, 
+             emoji: { 
+               id: null, 
+               name: '🗑' 
+             }, 
+             type: 2 
+           } 
+         ] 
+       } 
+     ], 
+     embeds: [ 
+       { 
+         type: 'rich', 
+         description: `**[${img.title}](${img.originalUrl})**`, 
+         title: ` Detected: ${images.search}`, 
+         color: getColor(img), 
+         image: { 
+           url: img.url, 
+           height: img.height, 
+           width: img.width 
+         }, 
+         author: { 
+           name: 'Google Reverse Image Search', 
+           icon_url: 'https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-1024.png' 
+         },
+        footer: {
+        text: `viewing page- \`1/${images.result.length}\``,
+       }
+       } 
+     ] 
+   }; 
+    let sent = await mseg.edit(msg);
+    await client.sleep(30200); 
+    if(!client.keyv.has(mseg.id)) { 
+      try {
+       await mseg.edit({   
+       content: '',   
+       embeds: msg.embeds,
+       components: []
+       });
+      } catch (e) {console.log(e.message);}
+     }
+  }
+};
+
+function getColor(image) {
+    const r = image.averageColorObject.r;
+    const g = image.averageColorObject.g;
+    const b = image.averageColorObject.b;
+    const color = (r << 16) | (g << 8) | b;  
+    return color;  
+}
