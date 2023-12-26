262ddf95e7eb8e19bba4cf2e67ab41782fc4ae8d
Sea-Deep <122673793+susudeepa@users.noreply.github.com>
2023-12-26 14:26:52 +0530

    Update wordle.js

--- PrefixCommands/games/wordle.js
+++ PrefixCommands/games/wordle.js
@@ -22,32 +22,41 @@ export default {
      components: [ 
        { 
          type: 1, 
-         components: [ 
-           { 
-             style: 1, 
-             label: `GUESS`, 
-             custom_id: `guessWordle`, 
-             disabled: false, 
-             emoji: { 
-               id: null, 
-               name: `🧐`, 
-             }, 
-             type: 2, 
-           },
-           { 
-             style: 4, 
-             label: `How to play?`, 
-             custom_id: `htpWordle`, 
-             disabled: false, 
-             emoji: { 
-               id: null, 
-               name: `❓`, 
-             }, 
-             type: 2, 
-           },            
-         ], 
-       }
-    ], 
+         components:  [
+          {
+            type: 1,
+            components: [
+              {
+                style: 1,
+                label: `ENTER`,
+                custom_id: `guessWordle`,
+                disabled: false,
+                emoji: {
+                  id: null,
+                  name: `🧐`,
+                },
+                type: 2,
+              },
+            ],
+          },
+           {
+            type: 1,
+            components: [
+              {
+                style: 4,
+                label: `How to play?`,
+                custom_id: `htpWordle`,
+                disabled: false,
+                emoji: {
+                  id: null,
+                  name: `❓`,
+                },
+                type: 2,
+              },
+             ],
+          },
+
+        ],
      embeds: [ 
        { 
          type: 'rich', 
