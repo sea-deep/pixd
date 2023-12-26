2a4d38e381f0601ac6a6949ac213fe9c7516263b
Sea-Deep <122673793+susudeepa@users.noreply.github.com>
2023-12-26 14:54:18 +0530

    Update wordle.js

--- PrefixCommands/games/wordle.js
+++ PrefixCommands/games/wordle.js
@@ -20,45 +20,40 @@ export default {
       let msg = await message.reply({ 
      content: `<@${message.author.id}>'s game`, 
      tts: false, 
-     components: [ 
-       { 
-         type: 1, 
-         components:  [
-          {
-            type: 1,
-            components: [
-              {
-                style: 1,
-                label: `ENTER`,
-                custom_id: `guessWordle`,
-                disabled: false,
-                emoji: {
-                  id: null,
-                  name: `🧐`,
-                },
-                type: 2,
-              },
-            ],
+     components: [
+    {
+      type: 1,
+      components: [
+        {
+          style: 1,
+          label: `ENTER`,
+          custom_id: `guessWordle`,
+          disabled: false,
+          emoji: {
+            id: null,
+            name: `🤔`
           },
-           {
-            type: 1,
-            components: [
-              {
-                style: 4,
-                label: `How to play?`,
-                custom_id: `htpWordle`,
-                disabled: false,
-                emoji: {
-                  id: null,
-                  name: `❓`,
-                },
-                type: 2,
-              },
-             ],
+          type: 2
+        }
+      ]
+    },
+    {
+      type: 1,
+      components: [
+        {
+          style: 4,
+          label: `How to play?`,
+          custom_id: `htpWordle`,
+          disabled: false,
+          emoji: {
+            id: null,
+            name: `❓`
           },
-     ],
-       },
-    ],  
+          type: 2
+        }
+      ]
+    }
+  ],  
      embeds: [ 
        { 
          type: 'rich', 
