125309ad7cf746bf598d450859ef7727e939c538
Sea-Deep <seadeepie@gmail.com>
2024-01-14 15:08:29 +0530

    Update tclookup.js

--- PrefixCommands/xuv/tclookup.js
+++ PrefixCommands/xuv/tclookup.js
@@ -5,7 +5,7 @@ export default {
   name: "truecaller",
   description: "Lookup Truecaller",
   aliases: ["tc"],
-  usage: "tc +91XXXXXXZZZX",
+  usage: "tc +91XXXXXZZZZZ",
   guildOnly: false,
   args: true,
   permissions: {
@@ -21,7 +21,7 @@ export default {
     return message.reply({
       content: "",
       embeds: [{
-        title: args.join(' '),
+        title: args.join(''),
         description: resp,
       }],
     });
@@ -29,27 +29,39 @@ export default {
 };
 
 async function lookup(number) {
+  const url = "https://search5-noneu.truecaller.com/v2/search";
+  const headers = {
+    "user-agent": "Truecaller/11.75.5 (Android;10)",
+    Authorization:
+      "Bearer "+ process.env['TRUECALLER'],
+  };
+  const params = {
+    q: number,
+    type: 4,
+    locAddr: "",
+    placement: "SEARCHRESULTS,HISTORY,DETAILS",
+    encoding: "json",
+  };
 
-    const searchData = {
-      number: number,
-      installationId: process.env['TRUECALLER']
-    };
-// console.log(searchData)
-   let response;
-    try {
-     response = await truecallerjs.search(searchData);
-} catch (e) {
-    console.log("Error occurred? ", e);
-  }
-   console.log(JSON.stringify(response,null, 2))
-    const r = response.data.data[0];
+  try {
+    const response = await fetch(
+      `${url}?${new URLSearchParams(params).toString()}`,
+      {
+        method: "GET",
+        headers: headers,
+      },
+    );
 
-    const data = [
+    const data = await response.json();
+    //  console.log(JSON.stringify(data.data[0], null, 2))
+    let r = data.data[0];
+    let out = [
       `**Name**: ${r?.name}`,
-      `**City**: ${r?.addresses[0]?.city}, ${response?.getCountryDetails()?.name} ${response?.getCountryDetails()?.flag}`,
+      `**City**: ${r?.addresses[0]?.city}, ${r?.phones[0].countryCode}`,
       `**Carrier**: ${r?.phones[0]?.carrier}`,
     ].join("\n");
-
-    return data;
-  
+    return out;
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
 }
