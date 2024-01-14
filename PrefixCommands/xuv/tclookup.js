18a820af3bcb1f5e161e7c59d02c8ad73c161270
Sea-Deep <seadeepie@gmail.com>
2024-01-14 13:49:45 +0530

    Update tclookup.js

--- PrefixCommands/xuv/tclookup.js
+++ PrefixCommands/xuv/tclookup.js
@@ -1,5 +1,5 @@
-import truecallerjs from "truecallerjs":
-import {Client, Message } from "discord.js";
+import truecallerjs from "truecallerjs";
+import { Client, Message } from "discord.js";
 
 export default {
   name: "truecaller",
@@ -13,21 +13,23 @@ export default {
     user: [],
   },
   /**
-    * @param {Message} message
-    * @param {Client} client
-    */
+   * @param {Message} message
+   * @param {Client} client
+   */
   execute: async (message, args, client) => {
-   let resp;
+    let resp;
     try {
-    resp = await lookup(args.join(' '));
-   } catch(e) {return message.reply(”An error occurred...\nMake sure you're putting number in the international format `+91XXXXXXXXXX`"}
+      resp = await lookup(args.join(' '));
+    } catch (e) {
+      return message.reply("An error occurred...?\nMake sure you're putting the number in the international format `+91XXXXXXXXXX`?");
+    }
     return message.reply({
-    content: 
-    embeds: [{
-     title: args.join(' '),
-     description: resp
-    }]
-   });
+      content: "",
+      embeds: [{
+        title: args.join(' '),
+        description: resp,
+      }],
+    });
   }
 };
 
@@ -35,21 +37,21 @@ async function lookup(number) {
   try {
     const searchData = {
       number: number,
-      installationId: "a1i0b--kMjadQkMkO4DOIYtdvS2AM2N2x-0Uu6PHN_M3Sd8LFaeNM9B6x76cVrHw",
+      installationId: process.env.TRUECALLER
     };
 
     const response = await truecallerjs.search(searchData);
 
-    const r = response.data.data[0];
+    const r = response?.data?.data[0];
 
     const data = [
       `**Name**: ${r?.name}`,
-      `**City**: ${r.addresses[0].city}, ${response.getCountryDetails().name} ${response.getCountryDetails().flag}`,
-      `**Carrier**: ${r.phones[0].carrier}`,
+      `**City**: ${r?.addresses[0]?.city}, ${response?.getCountryDetails()?.name} ${response?.getCountryDetails()?.flag}`,
+      `**Carrier**: ${r?.phones[0]?.carrier}`,
     ].join("\n");
 
     return data;
   } catch (e) {
-    throw new Error("Error occurred", e);
+    throw new Error("Error occurred? ", e);
   }
 }
