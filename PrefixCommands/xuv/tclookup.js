00be5b4ac3a886da1f5dc27a604f9ebd7dc17244
Sea-Deep <seadeepie@gmail.com>
2024-01-14 13:47:14 +0530

    Update tclookup.js

--- PrefixCommands/xuv/tclookup.js
+++ PrefixCommands/xuv/tclookup.js
@@ -1,4 +1,35 @@
 import truecallerjs from "truecallerjs":
+import {Client, Message } from "discord.js";
+
+export default {
+  name: "truecaller",
+  description: "Lookup Truecaller",
+  aliases: ["tc"],
+  usage: "tc +916289522067",
+  guildOnly: false,
+  args: true,
+  permissions: {
+    bot: [],
+    user: [],
+  },
+  /**
+    * @param {Message} message
+    * @param {Client} client
+    */
+  execute: async (message, args, client) => {
+   let resp;
+    try {
+    resp = await lookup(args.join(' '));
+   } catch(e) {return message.reply(”An error occurred...\nMake sure you're putting number in the international format `+91XXXXXXXXXX`"}
+    return message.reply({
+    content: 
+    embeds: [{
+     title: args.join(' '),
+     description: resp
+    }]
+   });
+  }
+};
 
 async function lookup(number) {
   try {
