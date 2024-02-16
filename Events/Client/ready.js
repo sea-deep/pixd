d167c4d47aac1f8fb4fdad358b440e5fd17f1a41
Sea-Deep <seadeepie@gmail.com>
2024-02-16 12:23:07 +0530

    Update ready.js

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -14,6 +14,7 @@ export default {
        name: `${status}`,
        type: ActivityType.Listening 
      });
+    async client.user.setAvatar("https://cdn.discordapp.com/attachments/1128597323895808020/1207939137722650654/doremon-smile.gif?ex=65e177fc&is=65cf02fc&hm=4b4e2afbd14e84f221f689c308fe3e5081e5aafc5f5fcdc88085d4a5b6f73287&");
   },
 };
 
