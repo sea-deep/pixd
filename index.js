d152dc75da14943d2693613c42aaf31a4e82e917
Sea-Deep <seadeepie@gmail.com>
2024-02-16 12:17:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,8 @@
    partials: [Partials.Channel], 
  }); 
   
+client.user.setAvatar("https://cdn.discordapp.com/attachments/1128597323895808020/1207939137722650654/doremon-smile.gif?ex=65e177fc&is=65cf02fc&hm=4b4e2afbd14e84f221f689c308fe3e5081e5aafc5f5fcdc88085d4a5b6f73287&");
+
  // Setting a Global Collection for Commands, Aliases, Buttons & Interactions and more 
  client.prefixCommands = new Collection(); 
  client.slashCommands = new Collection(); 
