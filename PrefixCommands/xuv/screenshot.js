458e17d123f03b8aa44d2f2bef7219f3b2c95f3d
Sea-Deep <seadeepie@gmail.com>
2024-02-17 16:43:33 +0530

    Update screenshot.js

--- PrefixCommands/xuv/screenshot.js
+++ PrefixCommands/xuv/screenshot.js
@@ -1,11 +1,11 @@
-import { Message, AttachmentBuilder} from "discord.js";
-import webshot from 'webshot';
+import { Message, AttachmentBuilder } from "discord.js-mobile";
+import captureWebsite from 'capture-website';
 
 export default {
   name: "screenshot",
   description: "Captures a screenshot of the specified website.",
   aliases: ["ss"],
-  usage: "screenshot <URL> [-m]",
+  usage: "!screenshot <URL> [-m]",
   guildOnly: true,
   args: false,
   permissions: {
@@ -22,29 +22,16 @@ export default {
 
     if (match && match[3]) {
       const url = (match[1] ? match[1] : 'https://') + (match[2] ? match[2] : '') + match[3];
-      let options = {};
-
-      if (isMobile) {
-        options = {
-          screenSize: {
-            width: 320,
-            height: 480
-          },
-          shotSize: {
-            width: 320,
-            height: 'all'
-          },
-          userAgent: 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.20 (KHTML, like Gecko) Mobile/7B298g'
-        };
-      }
 
       try {
-        const renderStream = await webshot(url, { streamType: 'png', ...options });
+        let screenshotBuffer;
+        if (isMobile) {
+          screenshotBuffer = await captureWebsite.buffer(url, { emulateDevice: 'iPhone X' });
+        } else {
+          screenshotBuffer = await captureWebsite.buffer(url);
+        }
 
-        renderStream.on('data', async function (data) {
-          const attachment = new AttachmentBuilder(data, 'screenshot.png');
-          await message.channel.send({content: 'Screenshot of <' + url + '>:', files: [ attachment ]});
-        });
+        await message.channel.send({ content: 'Screenshot of <' + url + '>:', files: [new AttachmentBuilder(screenshotBuffer, 'screenshot.png')] });
       } catch (error) {
         console.error('Error capturing screenshot:', error);
         await message.channel.send('Error capturing screenshot: ' + error.message);
