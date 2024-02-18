e9be4ca8e6108246648a4d483839cbbbcffa230e
Sea-Deep <seadeepie@gmail.com>
2024-02-18 22:34:18 +0530

    Update owner

---
+++ PrefixCommands/owner/donate.js
@@ -0,0 +1,24 @@
+import {Client, Message } from "discord.js";
+
+export default {
+  name: "donate",
+  description: "daan",
+  aliases: [""],
+  usage: "",
+  guildOnly: false,
+  args: false,
+  permissions: {
+    bot: [],
+    user: [],
+  },
+  /**
+    * @param {Message} message
+    * @param {Client} client
+    */
+  execute: async (message, args, client) => {
+    await message.reply({
+     content: "",
+     files: [new AttachmentBuilder("../../Assets/donate.mp4", "donate.mp4")]
+});
+  }
+};
