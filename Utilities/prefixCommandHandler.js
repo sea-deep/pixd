9940dcde4d8fa5476a130ff4ae5c930e209af149
Sea-Deep <seadeepie@gmail.com>
2024-02-28 19:27:56 +0530

    Update prefixCommandHandler.js

--- Utilities/prefixCommandHandler.js
+++ Utilities/prefixCommandHandler.js
@@ -11,7 +11,7 @@ try {
   
   for (let i = 0; i < Files.length; i++) {
     Files[i] = pathToFileURL(Files[i]);
-   console.log(Files[i])
+ //  console.log(Files[i])
     const commandFile = await import(Files[i]);
     const command = commandFile.default;
     if (command.name) {     client.prefixCommands.set(command.name, command);
