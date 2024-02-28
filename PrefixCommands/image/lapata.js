a6514ac03c4126939822bf94b75adf59b7453e00
Sea-Deep <seadeepie@gmail.com>
2024-02-28 14:21:35 +0530

    Update lapata.js

--- PrefixCommands/image/lapata.js
+++ PrefixCommands/image/lapata.js
@@ -16,17 +16,47 @@ export default {
     * @param {Message} message
     */
   execute: async (message) => {
-   let base = new Jimp(720, 404, 0x00000000); 
-   let avatar = await Jimp.read(await getInputImage(message)); 
-   avatar.resize(156, 182); 
-   let fg = await Jimp.read( 
-     'https://cdn.discordapp.com/attachments/916697198761234492/1104896270428020807/PicsArt_05-08-03.41.52.png' 
-   ); 
-   base.composite(avatar, 32, 119).composite(fg, 0, 0); 
+  let msg = await message.reply("Creating...");
+   let base = new Jimp(1928, 1322, 0x00000000); 
+    let overlays = [];
+    if (message.mentions.users.size > 1) {
+        overlays =message.mentions.parsedUsers.map((user) =>
+           user.displayAvatarURL({ extension: "png", size: 256, forceStatic:true})
+        );
+
+        const neededDuplicates = 5 - overlays.length;
+        if (neededDuplicates > 0) {
+            const duplicatedElements = overlays.slice(0, neededDuplicates);
+            overlays.push(...duplicatedElements);
+        }
+    } else {
+        let overlay = await getInputImage(message);
+        overlays = [overlay, overlay, overlay, overlay, overlay];
+    }
+    const s = [
+      {w: 359, h: 437, x: 145, y: 334},
+      {w: 195, h: 289, x: 938, y: 398},
+      {w: 117, h: 245, x: 1371, y: 451},
+      {w: 77, h: 206, x: 1634, y: 474},
+      {w: 59, h: 175, x: 1805, y: 477}
+    ]
+     console.log(overlays)
+
+    overlays.forEach(async (overlay, i) => {
+     let image = await Jimp.read(overlay);
+      image.resize(s[i].w, s[i].h);
+      base.composite(image, s[i].x, s[i].y);
+    });
+    
+   let fg = await Jimp.read('https://iili.io/JM9qUPf.md.png');    fg.resize(1928, 1322);
+   base.composite(fg, 0, 0); 
+
+    
    let buffer = await base.getBufferAsync(Jimp.MIME_PNG); 
    let file = new AttachmentBuilder(buffer, {name: 'lapata.png'}); 
+    await msg.delete();
    return message.reply({ 
-     comtent: '', 
+     content: '', 
      files: [file], 
    });
   }
