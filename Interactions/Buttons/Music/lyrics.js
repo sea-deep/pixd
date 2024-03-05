ad9ff7afb287bc5a5da6569e65bf3b7caf88fe10
Sea-Deep <seadeepie@gmail.com>
2024-03-05 19:38:42 +0530

    Update Buttons

---
+++ Interactions/Buttons/Music/lyrics.js
@@ -0,0 +1,28 @@
+import { getLyrics } from "fetch-lyrics";
+
+export default {
+  name: "lyrics",
+  execute: async (interaction) => {
+    let title = interaction.message.embeds[0].title.split(" - ")[1];
+
+    await interaction.deferReply({
+      ephemeral: true,
+    });
+    const call = await getLyrics(title);
+    const lyrics = call.lyrics;
+    const chunks = lyrics.match(/[\s\S]{1,3900}/g);
+
+    chunks.forEach(async (chunk) => {
+      await interaction.followUp({
+        content: "",
+        embeds: [
+          {
+            description: chunk,
+            color: 0xf0f0f0,
+          },
+        ],
+        ephemeral: true,
+      });
+    });
+  },
+};
