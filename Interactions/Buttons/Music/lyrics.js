aa893bfbc364236490180cb4c342782f06454c19
Sea-Deep <seadeepie@gmail.com>
2024-03-05 22:18:09 +0530

    Update lyrics.js

--- Interactions/Buttons/Music/lyrics.js
+++ Interactions/Buttons/Music/lyrics.js
@@ -1,4 +1,5 @@
 import { getLyrics } from "fetch-lyrics";
+import azlyrics from "azlyrics-lyric-scraper";
 
 export default {
   name: "getLyrics",
@@ -8,8 +9,8 @@ export default {
     await interaction.deferReply({
       ephemeral: true,
     });
-    const call = await getLyrics(title);
-    const lyrics = call.lyrics;
+    const call = await azlyrics.searchSong(title);
+    const lyrics = getLyrics(call.songs[0].url);
     const chunks = lyrics.match(/[\s\S]{1,3900}/g);
 
     chunks.forEach(async (chunk) => {
@@ -26,3 +27,27 @@ export default {
     });
   },
 };
+
+
+async function getLyrics(url) {
+   const r = await fetch(url);
+
+    if (!r.ok) {
+      throw new Error(`Failed to fetch lyrics. Status: ${r.status}`);
+    }
+    const htmlText = await r.text();
+    const indexOfComment = htmlText.indexOf(
+      "Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that."
+    );
+    const startIndex = htmlText.lastIndexOf("<div", indexOfComment);
+    const endIndex = htmlText.indexOf("</div>", indexOfComment) + 6;
+    const lyrics = htmlText
+    .substring(startIndex, endIndex)
+    .replace(/<!--[^>]*-->/g, "")
+    .replace(/<br>/g, "")
+    .replace(/<\/?div[^>]*>/g, "")
+    .replace(/<\/?i[^>]*>/g, "")
+    .trim();
+
+    return lyrics;
+}
