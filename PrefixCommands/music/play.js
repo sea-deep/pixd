20bba509e0baa04d8b3bc9a835267d830a438caa
Sea-Deep <seadeepie@gmail.com>
2024-03-07 16:45:40 +0530

    Update play.js

--- PrefixCommands/music/play.js
+++ PrefixCommands/music/play.js
@@ -219,7 +219,7 @@ export default {
                   color: 0x462,
                   author: {
                     name: `${song.title} - ${song.durationTime.minutes}:${song.durationTime.seconds}`,
-                    icon_url: `https://media.discordapp.net/attachments/1011986872500764672/1090737187869438033/icons8-cd.gif`,
+                    icon_url: `https://cdn.discordapp.com/emojis/763415718271385610.gif`,
                   },
                 },
               ],
@@ -236,7 +236,7 @@ export default {
                   color: 0x462,
                   author: {
                     name: `Added ${songs.length} songs to the queue`,
-                    icon_url: `https://media.discordapp.net/attachments/1011986872500764672/1090737187869438033/icons8-cd.gif`,
+                    icon_url: `https://cdn.discordapp.com/emojis/763415718271385610.gif`,
                   },
                 },
               ],
