ff5c6e9a3c826a00244920fbb92b483134a101a9
Sea-Deep <seadeepie@gmail.com>
2024-03-13 11:05:59 +0530

    sharp migration
    lapata

--- PrefixCommands/image/lapata.js
+++ PrefixCommands/image/lapata.js
@@ -1,6 +1,7 @@
 import { AttachmentBuilder, Message } from "discord.js";
-import Jimp from "jimp";
+import sharp from "sharp";
 import { getInputImage } from "../../Helpers/helpersImage.js";
+
 export default {
   name: "lapata",
   description: "Create a pk lapata image...",
@@ -13,51 +14,72 @@ export default {
     user: [],
   },
   /**
-    * @param {Message} message
-    */
+   * @param {Message} message
+   */
   execute: async (message) => {
-  let msg = await message.reply("Creating...");
-   let base = new Jimp(1928, 1322, 0x00000000); 
     let overlays = [];
     if (message.mentions.users.size > 1) {
-        overlays =message.mentions.parsedUsers.map((user) =>
-           user.displayAvatarURL({ extension: "png", size: 256, forceStatic:true})
-        );
+      overlays = message.mentions.parsedUsers.map((user) =>
+        fetch(
+          user.displayAvatarURL({
+            extension: "png",
+            size: 256,
+            forceStatic: true,
+          }),
+        )
+          .then((res) => res.arrayBuffer())
+          .catch((e) => {
+            console.log(e);
+            return null;
+          }),
+      );
 
-        const neededDuplicates = 5 - overlays.length;
-        if (neededDuplicates > 0) {
-            const duplicatedElements = overlays.slice(0, neededDuplicates);
-            overlays.push(...duplicatedElements);
-        }
+      const neededDuplicates = 5 - overlays.length;
+      if (neededDuplicates > 0) {
+        const duplicatedElements = overlays.slice(0, neededDuplicates);
+        overlays.push(...duplicatedElements);
+      }
     } else {
-        let overlay = await getInputImage(message);
-        overlays = [overlay, overlay, overlay, overlay, overlay];
+      let url = await getInputImage(message);
+      const res = await fetch(url);
+      const overlay = await res.arrayBuffer();
+      overlays = [overlay, overlay, overlay, overlay, overlay];
     }
     const s = [
-      {w: 359, h: 437, x: 145, y: 334},
-      {w: 195, h: 289, x: 938, y: 398},
-      {w: 117, h: 245, x: 1371, y: 451},
-      {w: 77, h: 206, x: 1634, y: 474},
-      {w: 59, h: 175, x: 1805, y: 477}
-    ]
-     console.log(overlays)
+      { w: 359, h: 437, x: 145, y: 334 },
+      { w: 195, h: 289, x: 938, y: 398 },
+      { w: 117, h: 245, x: 1371, y: 451 },
+      { w: 77, h: 206, x: 1634, y: 474 },
+      { w: 59, h: 175, x: 1805, y: 477 },
+    ];
 
-    overlays.forEach(async (overlay, i) => {
-     let image = await Jimp.read(overlay);
-      image.resize(s[i].w, s[i].h);
-      base.composite(image, s[i].x, s[i].y);
+    for (let i = 0; i < overlays.length; i++) {
+      overlays[i] = await sharp(overlays[i]).resize(s[i].w, s[i].h, { fit: "fill"}).toBuffer();
+    }
+
+    let lapata = await sharp({
+      create: {
+        height: 1322,
+        width: 1928,
+        channels: 4,
+        background: { r: 0, g: 0, b: 0, alpha: 0 },
+      },
+    })
+      .composite([
+        { input: overlays[0], top: s[0].y, left: s[0].x },
+        { input: overlays[1], top: s[1].y, left: s[1].x },
+        { input: overlays[2], top: s[2].y, left: s[2].x },
+        { input: overlays[3], top: s[3].y, left: s[3].x },
+        { input: overlays[4], top: s[4].y, left: s[4].x },
+        { input: "./Assets/lapata.png", top: 0, left: 0 },
+      ])
+      .png()
+      .toBuffer();
+
+    let file = new AttachmentBuilder(lapata, { name: "lapata.png" });
+    return message.reply({
+      content: "",
+      files: [file],
     });
-    
-   let fg = await Jimp.read('https://iili.io/JM9qUPf.png');    
-   base.composite(fg, 0, 0); 
-
-    
-   let buffer = await base.getBufferAsync(Jimp.MIME_PNG); 
-   let file = new AttachmentBuilder(buffer, {name: 'lapata.png'}); 
-    await msg.delete();
-   return message.reply({ 
-     content: '', 
-     files: [file], 
-   });
-  }
+  },
 };
