733e8132b45a5a5b5cdb94d1bc954215e9bd55ab
Sea-Deep <seadeepie@gmail.com>
2024-03-13 11:05:22 +0530

    sharp migration goodness

--- PrefixCommands/image/goodness.js
+++ PrefixCommands/image/goodness.js
@@ -1,8 +1,7 @@
 import { AttachmentBuilder, Message } from "discord.js";
-import Jimp from "jimp"; 
- import GIFEncoder from 'gif-encoder-2'; 
- import path from 'path'; 
- import { getInputImage } from "../../Helpers/helpersImage.js";
+import sharp from "sharp";
+import GIFEncoder from "gif-encoder-2";
+import { getInputImage } from "../../Helpers/helpersImage.js";
 export default {
   name: "goodness",
   description: "oh my goodness gracious",
@@ -15,31 +14,35 @@ export default {
     user: [],
   },
   /**
-    * @param {Message} message
-    */
+   * @param {Message} message
+   */
   execute: async (message) => {
-   let m = await message.reply('Processing...'); 
-   let avatar = await getInputImage(message); 
-   let av = await Jimp.read(avatar); 
-   av.resize(160,157) 
-   const encoder = new GIFEncoder(260, 296); 
-   encoder.setDelay(50); 
-   encoder.start(); 
-   for (let i = 0; i < 34; i++) { 
-     const frame = i < 10 ? `0${i}` : `${i}`; 
-     const file = path.resolve(`./Assets/goodness/frame_${frame}_delay-0.05s.gif`); 
-  
-     let banner = await Jimp.read(file); 
-     banner.composite(av, 101, 139); 
-     encoder.addFrame(banner.bitmap.data); 
-   } 
-   encoder.finish(); 
-   const buffer = encoder.out.getData(); 
-   let file = new AttachmentBuilder(buffer, {name: 'godnessgraciousness.gif'}); 
-   await message.reply({ 
-     content: ``, 
-     files: [file], 
-   }); 
-  await m.delete();
-  }
+    let m = await message.reply("Processing...");
+    let url = await getInputImage(message);
+    let res = await fetch(url);
+    let buffer = await res.arrayBuffer();
+    let avatar = await sharp(buffer).resize(160, 157).toBuffer();
+    const encoder = new GIFEncoder(260, 296);
+    encoder.setDelay(50);
+    encoder.start();
+    for (let i = 0; i < 34; i++) {
+      const frame = i < 10 ? `0${i}` : `${i}`;
+
+      let good = sharp(
+        `./Assets/goodness/frame_${frame}_delay-0.05s.gif`,
+      ).composite([{ input: avatar, top: 139, left: 101 }]);
+      const { data } = await good.raw().toBuffer({ resolveWithObject: true });
+      encoder.addFrame(data);
+    }
+    encoder.finish();
+    const goodness = encoder.out.getData();
+    let file = new AttachmentBuilder(goodness, {
+      name: "godnessgraciousness.gif",
+    });
+    await message.reply({
+      content: ``,
+      files: [file],
+    });
+    await m.delete();
+  },
 };
