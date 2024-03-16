752da466597f1538926d7cf13b2e393fb1d19dc9
Sea-Deep <seadeepie@gmail.com>
2024-03-16 21:48:34 +0530

    update Interactions/Buttons/2048/arrowLeft.js

--- Interactions/Buttons/2048/arrowLeft.js
+++ Interactions/Buttons/2048/arrowLeft.js
@@ -3,6 +3,8 @@ import { move, message2048, parseDesc, calculateScore } from "../../../Helpers/h
 export default {
   name: '2048left',
   execute: async(interaction) => {
+   await interaction.deferUpdate();
+        
       const description = interaction.message.embeds[0].description; 
    let newDescription = move(description, 'left'); 
   
@@ -10,7 +12,6 @@ export default {
      description: newDescription, 
      score: calculateScore(parseDesc(newDescription)), 
    }); 
-    await interaction.deferUpdate();
    return interaction.message.edit(msg);
   }
 }
