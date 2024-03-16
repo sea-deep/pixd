fa4c562e921962dbb675e292845295539f92f03a
Sea-Deep <seadeepie@gmail.com>
2024-03-16 22:39:00 +0530

    update Interactions/Buttons/Wordle/getWordDefinition.js

--- Interactions/Buttons/Wordle/getWordDefinition.js
+++ Interactions/Buttons/Wordle/getWordDefinition.js
@@ -2,19 +2,25 @@ import { Client } from 'discord.js';
 import axios from "axios";
 export default {
   name: 'getWordDef',
-  deferReply: true,
   /**
   * @param {Client} client
   */
   execute: async (interaction, client) => {
+    await interaction.deferReply({
+      ephemeral: false
+    });
   let word = client.keyv.get(interaction.message.id);
     let key = process.env.DICTIONARY_API_KEY;
    let resp = await axios( 
 `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${key}`);
 
    let shortdef = resp.data[0].shortdef ? '- **' + resp.data[0].shortdef.join('**\n- **') + '**' : '`couldn’t find lmao`'; 
-   return interaction.followUp( 
-     `The definitions for \`${word}\` are:\n${shortdef}` 
-   );
+   return interaction.followUp({
+     content: '',
+     embeds: [{
+       description: `The definitions for \`${word}\` are:\n${shortdef}`,
+       color: client.color
+     }]
+   });
   }
 };
