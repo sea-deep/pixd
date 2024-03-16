8fc8930279a0544f63cc75b7208b02628b04c924
Sea-Deep <seadeepie@gmail.com>
2024-03-16 19:37:50 +0530

    update Interactions/Buttons/Music/lyrics.js

--- Interactions/Buttons/Music/lyrics.js
+++ Interactions/Buttons/Music/lyrics.js
@@ -2,12 +2,10 @@ import { getLyrics, searchSong  } from "../../../Helpers/helpersMusic.js";
 
 export default {
   name: "getLyrics",
+  deferReply: true,
   execute: async (interaction) => {
     let title = interaction.message.embeds[0].title.split(" - ")[1];
-
-    await interaction.deferReply({
-      ephemeral: true,
-    });
+    
     const call = await searchSong(title);
     const lyrics = await getLyrics(call.songs[0].url);
  //  console.log(lyrics)
