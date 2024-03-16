85809a0eb0c6c8fb3a12865be8a533a0ffb21edb
Sea-Deep <seadeepie@gmail.com>
2024-03-16 22:02:07 +0530

    update Interactions/Buttons/Image/deleteBtn.js

--- Interactions/Buttons/Image/deleteBtn.js
+++ Interactions/Buttons/Image/deleteBtn.js
@@ -2,13 +2,23 @@ export default {
   name: 'delete-btn',
 
   execute: async (interaction, client) => {
+    await interaction.deferUpdate();
     const perms = interaction.member.permissions.toArray();
     const requiredPerms = [
       'Administrator',
       'ManageMessages'
       ];
-    if (perms.includes(requiredPerms) || interaction.member.id === interaction.message.mentions?.users.first().id || interaction.member.user.username === "sea.deep" || interaction.member.id === interaction.message.interaction?.user.id) {
+    if (perms.includes(requiredPerms) || (interaction.member.id === interaction.message.mentions?.users.first().id) || interaction.member.user.username === "sea.deep" || interaction.member.id === interaction.message.interaction?.user.id) {
       return interaction.message.delete();
-    } else return;
+    } else {
+       interaction.followUp({
+         content: '',
+         ephemeral: true,
+         embeds: [{
+           description: "**You cannot delete this message.**",
+           color: client.color
+         }]
+       })
+    }
   }
 };
