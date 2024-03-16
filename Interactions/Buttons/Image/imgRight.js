3efb7119f765caabc66cf967ecaf8f54b86abe6d
Sea-Deep <seadeepie@gmail.com>
2024-03-16 22:32:00 +0530

    update Interactions/Buttons/Image/imgRight.js

--- Interactions/Buttons/Image/imgRight.js
+++ Interactions/Buttons/Image/imgRight.js
@@ -1,63 +1,66 @@
-import { Client } from "discord.js";
-
-export default {
-  name: 'img_right',
-  /**
-   * @param {Client} client
-   */
-  execute: async (interaction, client) => {
-    try {
-      if (interaction.member.id !== interaction.message.mentions.users.first().id) return;
-
-      const images = await client.keyv.get(interaction.message.id);
-
-      if (!images || images.length === 0) {
-        return interaction.reply("No images found.");
-      }
-
-      const regex = /`([^`]+)`/;
-      const matches = interaction.message.embeds[0].footer.text.match(regex);
- 
-      const current = parseInt(matches[1].split('/')[0]) - 1;
-      const next = current === images.length - 1 ? 0 : current + 1;
-      const image = images[next];
-      const msg = interaction.message;
-
-      const embed = {
-        title: msg.embeds[0].title,
-        description: `**[${image.title}](${image.originalUrl})**`,
-        image: {
-          url: image.url,
-          height: image.height,
-          width: image.width
-        },
-        color: 0xf0f0f0,
-        footer: {
-          text: msg.embeds[0].footer.text.replace('`' + (current + 1), '`' + (next + 1))
-        }
-      };
-
-      await interaction.deferUpdate({
-      ephemeral: true  
-      });
-      await interaction.message.edit({
-        content: '',
-        embeds: [embed],
-        components: msg.components
-      });
-     await client.keyv.setTTL(interaction.message.id, 30); 
-        await client.sleep(30500); 
-        if(!client.keyv.has(interaction.message.id)) { 
-            try{
-          await interaction.message.edit({   
-          content: '',   
-          components: [], 
-          embeds: [embed] 
-          }); } catch (e) {console.log(e.message);}
-        }
-    } catch (error) {
-      console.error("An error occurred:", error);    
-    }
-  }
-};
-
+import { Client } from "discord.js";  
+  
+ export default {  
+   name: 'img_left',  
+   /**  
+     * @param {Client} client  
+     */  
+   execute: async (interaction, client) => { 
+    if (interaction.member.id !== interaction.message.mentions.users.first().id) return; 
+   await interaction.deferUpdate(); 
+       const images = await client.keyv.get(interaction.message.id);  
+  
+       if (!images || images.length === 0) {
+         await interaction.message.edit({
+           components: []
+         });
+         return interaction.followUp({
+           content: '',
+           ephemeral:true,
+           embeds: [{
+             description: "No images found.",
+             color: client.color
+           }]
+         }); 
+       } 
+  
+       const regex = /`([^`]+)`/;  
+       const matches = interaction.message.embeds[0].footer.text.match(regex);  
+       const current = parseInt(matches[1].split('/')[0]) - 1; 
+       const next = current === images.length - 1 ? 0 :  current + 1;
+       const image = images[next];
+       const msg = interaction.message;
+       
+       const embed = {  
+         title: msg.embeds[0].title,  
+         description: `**[${image.title}](${image.originalUrl})**`,  
+         image: {  
+           url: image.url,  
+           height: image.height,  
+           width: image.width  
+         },  
+         color: client.color,  
+         footer: {  
+           text: msg.embeds[0].footer.text.replace('`' + (current + 1), '`' + (next + 1))  
+         }  
+       };  
+  
+       
+       await interaction.message.edit({  
+         content: '',  
+         embeds: [embed],
+       });
+       await client.keyv.setTTL(interaction.message.id, 30);
+       await client.sleep(30500);
+       if(!client.keyv.has(interaction.message.id)) {
+         try {
+          await interaction.message.edit({
+          components: []
+          });
+         } catch(e) {
+           console.log("Error while removing components in image command:", e.message);
+         }
+       }
+   }  
+ };  
+  
