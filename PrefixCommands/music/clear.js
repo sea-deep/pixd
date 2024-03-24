5ddf1d8dffc7350ae7a4b9b911af3d919825398d
Sea-Deep <seadeepie@gmail.com>
2024-03-24 09:49:47 +0530

    update PrefixCommands/music/clear.js

--- PrefixCommands/music/clear.js
+++ PrefixCommands/music/clear.js
@@ -18,10 +18,34 @@ export default {
   execute: async (message, args, client) => {
     let serverQueue = client.queue.get(message.guild.id);
     if (!message.member.voice.channel) {
-    return message.react('<:error:1090721649621479506>');
+        let er = await message.reply({
+          content: '',
+          embeds: [
+            {
+              author: {
+                name: '❌ Please join a  voice channel first.',
+              },
+              color: client.color,
+            },
+          ],
+        });
+        await client.sleep(5000);
+        return deleteMessage(er);
   }
   if (!serverQueue || serverQueue.songs.length == 0) {
-    return message.react('<:error:1090721649621479506>');
+        let er = await message.reply({
+          content: '',
+          embeds: [
+            {
+              author: {
+                name: '❌ No song is present in the queue.',
+              },
+              color: client.color,
+            },
+          ],
+        });
+        await client.sleep(5000);
+        return deleteMessage(er);
   }
 
   let currentSong = serverQueue.songs[0];
@@ -31,3 +55,11 @@ export default {
   return message.react('<:clear:1090718705060684008>');
   }
 };
+
+async function deleteMessage(msg) {
+  try {
+    return msg.delete();
+  } catch (e) {
+    return;
+  }
+}
