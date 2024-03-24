b71777eea8d75609285cd4f7f2b43d0b2dba0dbe
Sea-Deep <seadeepie@gmail.com>
2024-03-24 11:04:40 +0530

    update PrefixCommands/music/resume.js

--- PrefixCommands/music/resume.js
+++ PrefixCommands/music/resume.js
@@ -18,10 +18,34 @@ export default {
   execute: async (message, args, client) => {
     let serverQueue = client.queue.get(message.guild.id);
   if (!message.member.voice.channel) {
-    return message.react('<:error:1090721649621479506>');
+    let er = await message.reply({
+          content: '',
+          embeds: [
+            {
+              author: {
+                name: '❌ Please join a  voice channel first.',
+              },
+              color: client.color,
+            },
+          ],
+        });
+        await client.sleep(5000);
+        return deleteMessage(er);
   }
   if (!serverQueue || serverQueue.songs.length == 0) {
-    return message.react('<:error:1090721649621479506>');
+               let er = await message.reply({
+          content: '',
+          embeds: [
+            {
+              author: {
+                name: '❌ No song to resum.',
+              },
+              color: client.color,
+            },
+          ],
+        });
+        await client.sleep(5000);
+        return deleteMessage(er);
   }
   console.log(`Song resumed.`);
   serverQueue.player.unpause();
@@ -29,3 +53,12 @@ export default {
 
   }
 };
+
+async function deleteMessage(msg) {
+  try {
+    return msg.delete();
+  } catch (e) {
+    return;
+  }
+}
+
