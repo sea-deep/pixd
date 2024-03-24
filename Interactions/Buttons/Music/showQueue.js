d09e697e752ba8ef99d504201c3a5be68b783900
Sea-Deep <seadeepie@gmail.com>
2024-03-24 12:23:41 +0530

    update Interactions/Buttons/Music/showQueue.js

---
+++ Interactions/Buttons/Music/showQueue.js
@@ -0,0 +1,28 @@
+export default {
+  name: "showQueue",
+  execute: async (interaction, client) => {
+    await interaction.deferReply({
+      ephemeral: true
+    });
+    let serverQueue = client.queue.get(interaction.guild.id);
+    let remaining = serverQueue.songs.slice(2); 
+    let chunks = [];
+
+    for (let i = 0; i < remaining.length; i += 20) {
+      let chunk = remaining.slice(i, i + 20);
+      let song = "";
+      chunk.forEach((item, index) => song += `${i + index + 2}. ${item}\n`); 
+      chunks.push(song); 
+    }
+    
+    for (let chunk of chunks) {
+      await interaction.followUp({
+        content: '',
+        embeds: [{
+          description: chunk,
+          color: client.color
+        }]
+      });
+    }
+  }
+};
