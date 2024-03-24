0a41c167d349849dec0dcd0eaf8ef34384552ce4
Sea-Deep <seadeepie@gmail.com>
2024-03-24 09:53:07 +0530

    update PrefixCommands/music/pause.js

--- PrefixCommands/music/pause.js
+++ PrefixCommands/music/pause.js
@@ -18,14 +18,46 @@ export default {
   execute: async (message, args, client) => {
     let serverQueue = client.queue.get(message.guild.id);
   if (!message.member.voice.channel) {
-    return message.react('<:error:1090721649621479506>');
+    let er = await message.reply({
+          content: '',
+          embeds: [
+            {
+              author: {
+                name: '❌ Please join a  voice channel first.',
+              },
+              color: client.color,
+            },
+          ],
+        });
+        await client.sleep(5000);
+        return deleteMessage(er);
   }
   if (!serverQueue || serverQueue.songs.length == 0) {
-    return message.react('<:error:1090721649621479506>');
+           let er = await message.reply({
+          content: '',
+          embeds: [
+            {
+              author: {
+                name: '❌ No song to pause.',
+              },
+              color: client.color,
+            },
+          ],
+        });
+        await client.sleep(5000);
+        return deleteMessage(er);
   }
-  console.log(`Song paused.`);
   serverQueue.player.pause();
   return message.react('<:pause:1090718191824683038>');
 
   }
 };
+
+
+async function deleteMessage(msg) {
+  try {
+    return msg.delete();
+  } catch (e) {
+    return;
+  }
+}
