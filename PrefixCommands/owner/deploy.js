58f0ef366b892b2ca960db82fc52ef746cd26c36
Sea-Deep <seadeepie@gmail.com>
2024-03-25 03:01:39 +0530

    update PrefixCommands/owner/deploy.js

---
+++ PrefixCommands/owner/deploy.js
@@ -0,0 +1,26 @@
+import {Client, Message } from "discord.js";
+
+export default {
+  name: "deploy",
+  description: "",
+  aliases: [""],
+  usage: "",
+  guildOnly: true,
+  args: false,
+  permissions: {
+    bot: [],
+    user: [],
+  },
+  /**
+    * @param {Message} message
+    * @param {Client} client
+    */
+  execute: async (message, args, client) => {
+    if (message.author.id == "908287391217905684") {
+  //  console.log(getInputImage(message))
+    await fetch(process.env.DEPLOY_HOOK);
+    await client.destroy();
+    return message.reply('Deploy started!');
+    } 
+  }
+};
