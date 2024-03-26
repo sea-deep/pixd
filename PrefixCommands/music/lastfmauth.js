c73e186bc8692dadbf50d539571200df72a1d086
Sea-Deep <seadeepie@gmail.com>
2024-03-26 21:24:51 +0530

    update PrefixCommands/music/lastfmauth.js

---
+++ PrefixCommands/music/lastfmauth.js
@@ -0,0 +1,67 @@
+import { Client, Message } from "discord.js";
+
+export default {
+  name: "lastfm",
+  description: "Login with Last.fm",
+  aliases: ["lf"],
+  usage: "",
+  guildOnly: true,
+  args: false,
+  permissions: {
+    bot: [],
+    user: [],
+  },
+  /**
+   * @param {Message} message
+   * @param {string[]} args
+   * @param {Client} client
+   */
+  execute: async (message, args, client) => {
+    const baseUrl = 'http://www.last.fm/api/auth/';
+    const params = new URLSearchParams({
+      api_key: process.env.LASTFM_KEY,
+      cb: 'https://pixd.onrender.com/lastfm/login',
+    });
+
+    const authUrl = `${baseUrl}?${params.toString()}`;
+    try {
+      await message.member.send({
+        content: "",
+        embeds: [{
+          description: "**Click the button below to login to your Last.fm account with PIXD."
+        }],
+        components: [
+          {
+            type: 1,
+            components: [
+              {
+                style: 5,
+                label: `LOGIN WITH LAST.FM`,
+                url: authUrl,
+                disabled: false,
+                type: 2
+              }
+            ]
+          }
+        ]
+      });
+      return message.reply({
+        content: "",
+        embeds: [{
+          title: "Just sent you a DM."
+        }]
+      });
+    } catch (e) {
+      return message.reply({
+        content: "",
+        embeds: [{
+          title: "An error occurred while sending you a DM",
+          description: e.message,
+          footer: {
+            text: "Make sure you have DMs enabled",
+          }
+        }]
+      });
+    }
+  }
+};
