6cb38b5a72f077f0f06952f635dac263bfcc3204
Sea-Deep <seadeepie@gmail.com>
2024-03-26 21:46:38 +0530

    update Helpers/helpersLastFm.js

---
+++ Helpers/helpersLastFm.js
@@ -0,0 +1,26 @@
+export async function handleLastFmAuth(req,res) {
+  try {
+    const params = new URLSearchParams({
+      method: 'auth.getSession',
+      api_key: process.env.LASTFM_KEY,
+      token: req.query.token,
+      format: 'json'
+    });
+
+    const response = await fetch(`http://ws.audioscrobbler.com/2.0/?${params.toString()}`);
+    const data = await response.json();
+
+    const { session } = data;
+    let accessToken;
+    if (session && session.key) {
+      accessToken = session.key;
+    } else {
+      throw new Error('Access token not found in response');
+    }
+  } catch (error) {
+    console.error('Error exchanging token for access token:', error);
+    res.status(500).send('An error occurred while exchanging token for access token');
+    throw error;
+  }
+  
+}
