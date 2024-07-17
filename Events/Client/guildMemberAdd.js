2916b0b11a6f59bbcbe30167b3ab1e09cfd3b4e2
Sea-Deep <seadeepie@gmail.com>
2024-07-17 14:22:46 +0530

    Update guildMemberAdd.js

--- Events/Client/guildMemberAdd.js
+++ Events/Client/guildMemberAdd.js
@@ -18,60 +18,76 @@ export default {
       default:
         break;
     }
-    async function sendOkbb(member, client) {
-      let avatarURL = member.user.displayAvatarURL({
-        extension: "png",
-        forceStatic: true,
-      });
-      let res = await fetch(avatarURL);
-      let buffer = await res.arrayBuffer();
-      const avatar = await sharp(buffer)
-        .resize(92, 92, {
-          background: { r: 0, g: 0, b: 0, alpha: 0 },
-        })
-        .toBuffer();
-      let text = `@${member.user.username} just joined the server`;
-      let channel_id = "1128609011852390400";
-      const encoder = new GIFEncoder(427, 320);
-      encoder.setDelay(100);
-      encoder.start();
-      for (let i = 0; i < 33; i++) {
-        const frame = i < 10 ? `0${i}` : `${i}`;
-        const bg = `./Assets/okbhaibudbak/frame_${frame}_delay-0.1s.gif`;
-        const banner = sharp(bg).composite([
-          { input: avatar, top: 6, left: 6 },
-          {
-            input: {
-              text: {
-                text: text,
-                fontfile: "./Assets/futura.otf",
-                width: 315,
-                height: 92,
-                align: "center",
-          //      justify: true,
-                font: "Futura Condensed Bold",
-              },
-            },
-            blend: "difference",
-            top: 6,
-            left: 104,
+
+async function sendOkbb(member, client) {
+  // Fetch and process avtar..
+  let avatarURL = member.user.displayAvatarURL({
+    extension: "png",
+    forceStatic: true,
+  });
+  let res = await fetch(avatarURL);
+  let buffer = await res.arrayBuffer();
+  const avatar = await sharp(buffer)
+    .resize(92, 92, {
+      background: { r: 0, g: 0, b: 0, alpha: 0 },
+    })
+    .toBuffer();
+
+  // Prepare text
+  let text = `@${member.user.username} just joined the server`;
+
+  // Prepare encoder
+  const encoder = new GIFEncoder(427, 320);
+  encoder.setDelay(100);
+  encoder.start();
+
+  // Preload assets and font data
+  const frames = Array.from({ length: 33 }, (_, i) =>
+    i < 10 ? `0${i}` : `${i}`
+  );
+  const promises = frames.map((frame) =>
+    sharp(`./Assets/okbhaibudbak/frame_${frame}_delay-0.1s.gif`).composite([
+      { input: avatar, top: 6, left: 6 },
+      {
+        input: {
+          text: {
+            text: text,
+            fontfile: "./Assets/futura.otf",
+            width: 315,
+            height: 92,
+            align: "center",
+            font: "Futura Condensed Bold",
           },
-        ]);
-        const { data } = await banner
-          .raw()
-          .toBuffer({ resolveWithObject: true });
-        encoder.addFrame(data);
-      }
-      encoder.finish();
-      const welcome = encoder.out.getData();
-      let file = new AttachmentBuilder(welcome, { name: "tofoquboolkaro.gif" });
-      let channel = client.channels.cache.get(channel_id);
-      await channel.send({
-        content: `Namaste saar <@${member.user.id}> cummed in sarvar`,
-        files: [file],
-      });
-    }
-    async function sendPajeet(member, client) {
+        },
+        blend: "difference",
+        top: 6,
+        left: 104,
+      },
+    ])
+  );
+
+  // Process frames in parallel
+  const banners = await Promise.all(promises);
+
+  // Add frames to encoder
+  for (const banner of banners) {
+    const { data } = await banner.raw().toBuffer({ resolveWithObject: true });
+    encoder.addFrame(data);
+  }
+
+  // Finish encoding
+  encoder.finish();
+  const welcome = encoder.out.getData();
+
+  // Send the result to the Discord channel
+  let channel = client.channels.cache.get("1128609011852390400");
+  let file = new AttachmentBuilder(welcome, { name: "tofoquboolkaro.gif" });
+  await channel.send({
+    content: `Namaste saar <@${member.user.id}> cummed in sarvar`,
+    files: [file],
+  });
+}
+   async function sendPajeet(member, client) {
       const rounded = Buffer.from(
         '<svg><rect x="0" y="0" width="50" height="50" rx="50" ry="50"/></svg>',
       );
