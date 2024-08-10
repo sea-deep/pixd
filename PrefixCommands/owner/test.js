9c3f03ec48749462187281f433995c3d4150704f
Deepak. <seadeepie@gmail.com>
2024-08-11 00:31:31 +0530

    Update test.js

--- PrefixCommands/owner/test.js
+++ PrefixCommands/owner/test.js
@@ -1,1 +1,25 @@
-// nothing
+import {Client, Message } from "discord.js";
+
+export default {
+  name: "say",
+  description: "Say something w Bot",
+  aliases: [""],
+  usage: "",
+  guildOnly: true,
+  args: false,
+  permissions: {
+    bot: [],
+    user: [],
+  },
+  /**
+    * @param {Message} message
+    * @param {Client} client
+    */
+  execute: async (message, args, client) => {
+   try{
+    let m = await message.channel.send(`teri maa randi`);
+    await client.sleep(5000);
+    await m.delete();
+   } catch(e) { console.log("Say Command Error handled");}
+  }
+};
