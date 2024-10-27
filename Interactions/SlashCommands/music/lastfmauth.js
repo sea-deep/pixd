74635436e8483397e72fef8d14b02de429905bd5
sea-deep <seadeepie@gmail.com>
2024-10-27 09:48:42 +0530

    Auto Update
    R  SlashCommands/SubCommands/hello-world.js -> Interactions/SlashCommands/SubCommands/hello-world.js
    R  "SlashCommands/XUV subcommands/genesis.js" -> "Interactions/SlashCommands/XUV subcommands/genesis.js"
    R  "SlashCommands/XUV subcommands/gpt.js" -> "Interactions/SlashCommands/XUV subcommands/gpt.js"
    R  "SlashCommands/XUV subcommands/padhaku.js" -> "Interactions/SlashCommands/XUV subcommands/padhaku.js"
    R  "SlashCommands/XUV subcommands/ytsum.js" -> "Interactions/SlashCommands/XUV subcommands/ytsum.js"
    R  SlashCommands/XUV.js -> Interactions/SlashCommands/XUV.js
    R  SlashCommands/contact.js -> Interactions/SlashCommands/contact.js
    R  SlashCommands/download.js -> Interactions/SlashCommands/download.js
    R  SlashCommands/hello.js -> Interactions/SlashCommands/hello.js
    R  SlashCommands/help.js -> Interactions/SlashCommands/help.js
    R  "SlashCommands/image subcommands/allustuff.js" -> "Interactions/SlashCommands/image subcommands/allustuff.js"
    R  "SlashCommands/image subcommands/animan.js" -> "Interactions/SlashCommands/image subcommands/animan.js"
    R  "SlashCommands/image subcommands/goodness.js" -> "Interactions/SlashCommands/image subcommands/goodness.js"
    R  "SlashCommands/image subcommands/lapata.js" -> "Interactions/SlashCommands/image subcommands/lapata.js"
    R  "SlashCommands/image subcommands/nearyou.js" -> "Interactions/SlashCommands/image subcommands/nearyou.js"
    R  "SlashCommands/image subcommands/vosahihai.js" -> "Interactions/SlashCommands/image subcommands/vosahihai.js"
    R  SlashCommands/image.js -> Interactions/SlashCommands/image.js
    A  Interactions/SlashCommands/music/lastfmauth.js
    R  SlashCommands/music/loop.js -> Interactions/SlashCommands/music/loop.js
    R  SlashCommands/music/lyrics.js -> Interactions/SlashCommands/music/lyrics.js
    R  SlashCommands/music/pause.js -> Interactions/SlashCommands/music/pause.js
    R  SlashCommands/music/play.js -> Interactions/SlashCommands/music/play.js
    R  SlashCommands/music/queue.js -> Interactions/SlashCommands/music/queue.js
    R  SlashCommands/music/resume.js -> Interactions/SlashCommands/music/resume.js
    R  SlashCommands/music/seek.js -> Interactions/SlashCommands/music/seek.js
    R  SlashCommands/music/skip.js -> Interactions/SlashCommands/music/skip.js
    R  SlashCommands/music/stop.js -> Interactions/SlashCommands/music/stop.js
    R  SlashCommands/owner/donate.js -> Interactions/SlashCommands/owner/donate.js
    R  SlashCommands/owner/ping.js -> Interactions/SlashCommands/owner/ping.js
    M  PrefixCommands/games/gridhopper.js
    M  PrefixCommands/music/lastfmauth.js
    M  PrefixCommands/utils/help.js
    D  SlashCommands/music/lastfm.js
    M  package-lock.json
    M  www/index.html

---
+++ Interactions/SlashCommands/music/lastfmauth.js
@@ -0,0 +1,47 @@
+export default {
+    data: {
+        name: "lastfm",
+        description: "Login with Last.fm ",
+    },
+    execute: async (interaction, client) => {
+        await interaction.deferReply({
+            ephemeral: true,
+        });
+        const baseUrl = "http://www.last.fm/api/auth/";
+        const params = new URLSearchParams({
+            api_key: process.env.LASTFM_KEY,
+            cb: `https://pixd.up.railway.app/lastfm/login?userid=${message.author.id}`,
+        });
+
+        const authUrl = `${baseUrl}?${params.toString()}`;
+        try {
+            return interaction.followUp({
+                ephemeral: true,
+                content: "",
+                embeds: [
+                    {
+                        description:
+                            "**Click the button below to authorise your Last.fm account with PIXD.**",
+                        color: client.color,
+                    },
+                ],
+                components: [
+                    {
+                        type: 1,
+                        components: [
+                            {
+                                style: 5,
+                                label: `LOGIN WITH LAST.FM`,
+                                url: authUrl,
+                                disabled: false,
+                                type: 2,
+                            },
+                        ],
+                    },
+                ],
+            });
+        } catch (e) {
+            console.error("Error while sending ephemeral in /lastf command")
+        }
+    }
+}
--- PrefixCommands/games/gridhopper.js
+++ PrefixCommands/games/gridhopper.js
@@ -1,4 +1,4 @@
-import {Client, Message } from "discord.js";
+import { ButtonInteraction, Client, ComponentType, Message } from "discord.js";
 
 export default {
     name: "gridhopper",
@@ -16,41 +16,63 @@ export default {
      * @param {Client} client
      */
     execute: async (message, args, client) => {
-        let components = [
-            {
+        let components = [];
+        for (let i = 0; i < 5; i++) {
+            components.push({
                 type: 1,
-                components: [
-                    {
-                        style: 2,
-                        label: `ENTER`,
-                        custom_id: `guessWordle`,
-                        disabled: false,
-                        emoji: {
-                            id: null,
-                            name: `🖋️`,
-                        },
-                        type: 2,
-                    },
-                ],
-            },
-            {
-                type: 1,
-                components: [
-                    {
-                        style: 4,
-                        label: `How to play?`,
-                        custom_id: `htpWordle`,
-                        disabled: false,
-                        emoji: {
-                            id: null,
-                            name: `❓`,
-                        },
-                        type: 2,
-                    },
-                ],
-            },
-        ];
+                components: []
+            });
+            for (let j = 0; j < 5; j++) {
+                components[i].components[j] = {
+                    style: 2,
+                    label: `•`,
+                    custom_id: `grid_${i}${j}`,
+                    disabled: false,
+                    type: 2,
+                };
+            }
+        }
+        let sentMessage = await message.channel.send({
+            content: "",
+            components: components
+        });
 
-
+        const collector = sentMessage.createMessageComponentCollector({ componentType: ComponentType.Button, time: 60*5*1000 });
+        let currentScore = 1;
+        let row = 0, col = 0, prevRow = 0, prevCol = 0;
+       // let optionCount = 0, seconds = 0;
+        let firstMove = true;
+        /**
+         * @param {ButtonInteraction} interaction
+         */
+        collector.on('collect', interaction => {
+            console.log(interaction.component.data);
+            if (interaction.user.id !== message.member.user.id || interaction.component.data.label !== "•") return;
+            if (interaction.component.style == 2 && !firstMove) {
+                return gameOver();
+            }
+            [prevRow, prevCol] = [row, col];
+            [row, col] = getSquarePosition(interaction.component.data.custom_id);
+          /*  
+            if (firstMove) {
+                clickedSquare.innerHTML = currentScore++;
+                clickedSquare.value = "-";
+                highlightValidMoves();
+                firstMove = false;
+            } else if (clickedSquare.style.backgroundColor !== "white" && isValidMove(Math.abs(row - prevRow), Math.abs(col - prevCol))) {
+                clickedSquare.innerHTML = currentScore++;
+                clickedSquare.value = "-";
+                highlightValidMoves();
+            } else {
+                endGame();
+            }*/
+        });
+        
     }
 };
+
+
+function getSquarePosition(customId) {
+    const value = customId.split("_")[1];
+    return [Number(value[0]), Number(value[1])];
+}
